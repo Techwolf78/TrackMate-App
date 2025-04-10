af1ffb44e53c8cb6c932e53af6bf7ca1425a56a3
Techwolf78 <mrajaypawar207@gmail.com>
2025-04-10 15:49:03 +0530

    first commit

--- src/components/Placement/Page/placement.jsx
+++ src/components/Placement/Page/placement.jsx
@@ -1,15 +1,14 @@
-// Sales.jsx
-import React from 'react';
-import PlacementForm from '../Components/PlacementForm';  // Importing SalesForm component
+// Placement.jsx
+import PlacementForm from '../Components/PlacementForm';  // Importing PlacementForm component
 import PlacementFormSubmit from '../Components/PlacementFormSubmit';  // Importing Submit component
 
-function Sales() {
+function Placement() {
   return (
     <div className='bg-white font-inter '>
-      <PlacementForm />  {/* You can use SalesForm here */}
+      <PlacementForm />  {/* You can use PlacementForm here */}
       <PlacementFormSubmit />     {/* You can use Submit here */}
     </div>
   );
 }
 
-export default Sales;
+export default Placement;
